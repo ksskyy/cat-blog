2ac4da3be8c91a6d486e0b11223a73bc308965aa
ksskyy <149976697+ksskyy@users.noreply.github.com>
2024-01-12 12:41:12 -0800

    finish update part

--- posts/update.php
+++ posts/update.php
@@ -16,23 +16,34 @@ if(isset($_GET['upd_id'])){
     if(empty($_POST['title'])||empty($_POST['subtitle'])||empty($_POST['body'])){
         echo "please enter you blog content";
     }else{
+
+      unlink("images/" .$rows->img."");
         $title = $_POST['title'];
         $subtitle = $_POST['subtitle'];
         $body = $_POST['body'];
+        $img  = $_FILES['img']['name'];
+
+        $dir = 'images/'. basename($img);
  
 
-        $update = $conn->prepare("UPDATE posts SET title = :title,subtitle =:subtitle,body=:body WHERE id = '$id'");
+        $update = $conn->prepare("UPDATE posts SET title = :title,subtitle =:subtitle,body=:body, img=:img WHERE id = '$id'");
+
         $update->execute([
             ':title' => $title,
             ':subtitle' => $subtitle,
-            ':body' => $body
+            ':body' => $body,
+            ':img'  => $img
         ]);
-        header('location:http://localhost/clean-blog/index.php');
+
+        if(move_uploaded_file($_FILES['img']['tmp_name'], $dir)){
+          header('location:http://localhost/clean-blog/index.php');
+        }
+        // header('location:http://localhost/clean-blog/index.php');
     }
 
 }
 ?>
-            <form method="POST" action="update.php?upd_id=<?php echo $id;?>">
+            <form method="POST" action="update.php?upd_id=<?php echo $id;?>" enctype="multipart/form-data">
               
               <div class="form-outline mb-4">
                 <input type="text" name="title" value="<?php echo $rows->title;?>" id="form2Example1" class="form-control" placeholder="title" />
@@ -46,10 +57,10 @@ if(isset($_GET['upd_id'])){
               <div class="form-outline mb-4">
                 <input type="text" name="body" value="<?php echo $rows->body;?>" id="form2Example1" class="form-control" placeholder="body" />
             </div>
-
+            <?php echo "<img src='images/".$rows->img."' width=500px height=300px>"?>
               
              <div class="form-outline mb-4">
-                <input type="file" name="img" id="form2Example1" class="form-control" placeholder="image" />
+                <input type="file" name="img" id="form2Example1"  class="form-control" placeholder="image" />
             </div>
 
 
